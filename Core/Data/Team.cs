6449fa9617e9ccefa22f5c59f8294c50a4583724
Victor M. Suarez <svmnotn@gmail.com>
2015-08-22 23:23:26 -0400

    implement IComparable for teams

--- Core/Data/Team.cs
+++ Core/Data/Team.cs
@@ -2,11 +2,20 @@
   using System;
   using System.Drawing;
 
-  public class Team {
+  public class Team : IComparable<Team> {
     public string name;
     public Color color;
     public Font font;
     [NonSerialized]
     public int score;
+
+    public int CompareTo(Team other) {
+      if(score > other.score) {
+        return -1;
+      } else if(score < other.score) {
+        return 1;
+      }
+      return 0;
+    }
   }
 }
