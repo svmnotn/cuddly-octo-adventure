a2e90db56285ddbe7e84cbb0cbd670625b7e6322
Victor M. Suarez <svmnotn@gmail.com>
2015-08-22 23:25:05 -0400

    add current team stats to QuestionSelect

--- Game/UI/Controls/QuestionSelect.Designer.cs
+++ Game/UI/Controls/QuestionSelect.Designer.cs
@@ -24,6 +24,9 @@
     /// </summary>
     private void InitializeComponent() {
       this.questionsTable = new System.Windows.Forms.TableLayoutPanel();
+      this.structureTable = new System.Windows.Forms.TableLayoutPanel();
+      this.teamLabel = new System.Windows.Forms.Label();
+      this.structureTable.SuspendLayout();
       this.SuspendLayout();
       // 
       // questionsTable
@@ -31,21 +34,48 @@
       this.questionsTable.ColumnCount = 1;
       this.questionsTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
       this.questionsTable.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.questionsTable.Location = new System.Drawing.Point(5, 5);
+      this.questionsTable.Location = new System.Drawing.Point(3, 52);
       this.questionsTable.Name = "questionsTable";
       this.questionsTable.RowCount = 1;
       this.questionsTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-      this.questionsTable.Size = new System.Drawing.Size(490, 490);
+      this.questionsTable.Size = new System.Drawing.Size(484, 435);
       this.questionsTable.TabIndex = 0;
+      // 
+      // structureTable
+      // 
+      this.structureTable.ColumnCount = 1;
+      this.structureTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.structureTable.Controls.Add(this.teamLabel, 0, 0);
+      this.structureTable.Controls.Add(this.questionsTable, 0, 1);
+      this.structureTable.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.structureTable.Location = new System.Drawing.Point(5, 5);
+      this.structureTable.Name = "structureTable";
+      this.structureTable.RowCount = 2;
+      this.structureTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.structureTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+      this.structureTable.Size = new System.Drawing.Size(490, 490);
+      this.structureTable.TabIndex = 0;
+      // 
+      // teamLabel
+      // 
+      this.teamLabel.AutoSize = true;
+      this.teamLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.teamLabel.Location = new System.Drawing.Point(3, 0);
+      this.teamLabel.Name = "teamLabel";
+      this.teamLabel.Size = new System.Drawing.Size(484, 49);
+      this.teamLabel.TabIndex = 1;
+      this.teamLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
       // 
       // QuestionSelect
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Controls.Add(this.questionsTable);
+      this.Controls.Add(this.structureTable);
       this.Name = "QuestionSelect";
       this.Padding = new System.Windows.Forms.Padding(5);
       this.Size = new System.Drawing.Size(500, 500);
+      this.structureTable.ResumeLayout(false);
+      this.structureTable.PerformLayout();
       this.ResumeLayout(false);
 
     }
@@ -53,5 +83,7 @@
     #endregion
 
     private System.Windows.Forms.TableLayoutPanel questionsTable;
+    private System.Windows.Forms.TableLayoutPanel structureTable;
+    private System.Windows.Forms.Label teamLabel;
   }
 }
--- Game/UI/Controls/QuestionSelect.cs
+++ Game/UI/Controls/QuestionSelect.cs
@@ -8,12 +8,27 @@
   internal partial class QuestionSelect : UserControl {
 
     internal QuestionSelect(List<Topic> topics) {
-      Console.WriteLine(topics);
       InitializeComponent();
+      SetLabel();
+      SetQuestions(topics);
+    }
+
+    void SetLabel() {
+      var team = Program.main.archive.settings.teams[Program.main.currTeam];
+      teamLabel.Font = team.font;
+      teamLabel.ForeColor = team.color;
+      teamLabel.Text = "Team: " + team.name + Environment.NewLine;
+      teamLabel.Text += "Score: " + team.score;
+    }
+
+    void SetQuestions(List<Topic> topics) {
       questionsTable.Controls.Clear();
       questionsTable.RowStyles.Clear();
       questionsTable.ColumnStyles.Clear();
-      foreach(var t in topics) {
+      questionsTable.ColumnCount = topics.Count;
+      int columnIndex = 0;
+      foreach(var t in topics) {// Add a column per topic
+        int rowIndex = 0;
         questionsTable.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, (100f / topics.Count)));
         var rowPercent = 100f / (t.questions.Keys.Count + 1f);
         questionsTable.RowStyles.Add(new RowStyle(SizeType.Percent, rowPercent));
@@ -21,20 +36,26 @@
         title.Text = t.name;
         title.TextAlign = ContentAlignment.MiddleCenter;
         title.Dock = DockStyle.Fill;
-        questionsTable.Controls.Add(title);
-        foreach(var k in t.questions.Keys) {
+        questionsTable.Controls.Add(title, columnIndex, rowIndex);
+        rowIndex += 1;
+        foreach(var k in t.questions.Keys) {// Add a row per question value
           var q = t.GetQuestion(k);
-          questionsTable.RowStyles.Add(new RowStyle(SizeType.Percent, rowPercent));
-          var bttn = new Button();
-          bttn.Dock = DockStyle.Fill;
-          bttn.Text = "" + q.value;
-          bttn.Click += (object sender, EventArgs e) => {
-            var mode = new Question(q);
-            mode.Dock = DockStyle.Fill;
-            Program.main.Current = mode;
-          };
-          questionsTable.Controls.Add(bttn);
+          if(q != null) {
+            questionsTable.RowStyles.Add(new RowStyle(SizeType.Percent, rowPercent));
+            var bttn = new Button();
+            bttn.Dock = DockStyle.Fill;
+            bttn.BackColor = Color.LightGray;
+            bttn.Text = "" + q.value;
+            bttn.Click += (object sender, EventArgs e) => {
+              var mode = new QuestionPanel(q);
+              mode.Dock = DockStyle.Fill;
+              Program.main.Current = mode;
+            };
+            questionsTable.Controls.Add(bttn, columnIndex, rowIndex);
+            rowIndex += 1;
+          }
         }
+        columnIndex += 1;
       }
     }
   }
