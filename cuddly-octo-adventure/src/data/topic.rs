a83763083ab1bf13060fea5dea8b25c013218e01
Victor M. Suarez <svmnotn@gmail.com>
2016-05-28 13:11:28 -0400

    feat: defined question

--- cuddly-octo-adventure/src/data/topic.rs
+++ cuddly-octo-adventure/src/data/topic.rs
@@ -1,8 +1,15 @@
-// TODO Find definition of a topic!
-pub struct Topic {
+use super::Question;
 
+pub struct Topic {
+    name: String,
+    questions: Vec<Question>,
 }
 
 impl Default for Topic {
-    fn default() -> Topic { Topic {} }
+    fn default() -> Topic {
+        Topic {
+            name: "Hitch Hiker".to_owned(),
+            questions: Vec::new(),
+        }
+    }
 }
