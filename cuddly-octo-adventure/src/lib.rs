a73c0f6a311d6783ce23d1ba95a5d67e2efb8f86
Victor M. Suarez <svmnotn@gmail.com>
2016-05-29 15:55:02 -0400

    fix: re-export gtk and gdk

--- cuddly-octo-adventure/src/lib.rs
+++ cuddly-octo-adventure/src/lib.rs
@@ -1,7 +1,15 @@
 #![feature(custom_derive, plugin, question_mark)]
 #![plugin(serde_macros)]
 // TODO The whole thing
-extern crate gtk;
-extern crate gdk;
+extern crate gtk as gtk_rs;
+extern crate gdk as gdk_rs;
+
+pub mod gtk {
+  pub use gtk_rs::*;
+}
+
+pub mod gdk {
+  pub use gdk_rs::*;
+}
 
 pub mod cuddle;
